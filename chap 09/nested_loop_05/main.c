7a511ccf84f8ca48ba83c2e267c927eb4e8b6649
kavya-krishnan-paramasivan-programmer <kkp592006@gmail.com>
2024-10-23 20:50:22 +0530

    updated project "nested_loop_05"
    Creating a nested loop in C with CodeBlocks

--- chap 09/nested_loop_05/main.c
+++ chap 09/nested_loop_05/main.c
@@ -10,7 +10,21 @@ int main() {
     printf("Creating a nested loop in C with CodeBlocks\n\n");
 
     //Variable declaration and initialization
+    int inner_counter, outer_counter;
 
+    // The for loop starts here
+    for(outer_counter = 1; outer_counter < 6; outer_counter++)
+    {
+        for(inner_counter = 1; inner_counter < 6 - outer_counter ; inner_counter++)
+        {
+            printf(" ");
+        }
+        for(inner_counter = 1; inner_counter <= outer_counter ; inner_counter++)
+        {
+            printf(" %d ", inner_counter );
+        }
+        printf("\n");
+    }
 
     return 0;
 }
